8daa27af4a842dc3f9c3f3dd1277b472758b5297
bramz <brockramz@gmail.com>
2018-02-23 19:53:44 +0000

    Cut down verbosity with template liberals, removed messages for confligation fix

--- sql/V007__messages_create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    sendtime TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    content TEXT NOT NULL,
-    user TEXT NOT NULL,
-    discriminator INTEGER NOT NULL,
-    channel INTEGER NOT NULL,
-    server TEXT NOT NULL
-);
