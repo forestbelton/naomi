8f3a5cc43ffd3b38b34f5cfa31399bb138e797f1
bramz <brockramz@gmail.com>
2018-02-23 22:50:43 +0000

    Add indices for user/chan/server

--- sql/V007__messages_create.sql
+++ sql/V007__messages_create.sql
@@ -7,3 +7,7 @@ CREATE TABLE IF NOT EXISTS messages(
     channel INTEGER NOT NULL,
     server TEXT NOT NULL
 );
+
+CREATE UNIQUE INDEX user_index ON messages(user);
+CREATE UNIQUE INDEX channel_index ON messages(channel);
+CREATE UNIQUE INDEX server_index ON messages(server);
