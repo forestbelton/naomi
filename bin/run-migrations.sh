73f56647ab5d82c78adcfd2e8143c8a4bcf06fe4
Forest Belton <forest@homolo.gy>
2018-02-24 21:21:13 -0800

    Add script for running migrations

---
+++ bin/run-migrations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+FLYWAY_URL='https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-linux-x64.tar.gz'
+
+if ! ( which flyway > /dev/null ); then
+    cd ${HOME}
+
+    curl -v ${FLYWAY_URL}
+    tar -xvzf flyway-commandline-5.0.7-linux-x64.tar.gz
+    export PATH=${PATH}:${HOME}/flyway-commandline-5.0.7-linux-x64
+
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}/..
+
+flyway -configFile=./conf/flyway.conf migrate
