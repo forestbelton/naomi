efc66d4c5ced7f676452e934aaf3226d576ec7bf
Forest Belton <forest@homolo.gy>
2018-02-25 12:59:39 -0800

    Handle null balance

--- sql/V012__remove_old_tables.sql
+++ sql/V012__remove_old_tables.sql
@@ -1,5 +1,5 @@
 UPDATE users
-    SET balance = (SELECT balance FROM user_casebucks WHERE user = users.discord_name);
+    SET balance = (SELECT COALESCE(balance, 0) FROM user_casebucks WHERE user = users.discord_name);
 
 DROP TABLE user_casebucks;
 DROP TABLE admins;
