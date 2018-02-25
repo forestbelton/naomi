56cd21788d36f88e9eb8bbc2e7a5e1540cc8f4f0
Forest Belton <forest@homolo.gy>
2018-02-24 21:54:25 -0800

    Improve migration script

--- bin/run-migrations.sh
+++ bin/run-migrations.sh
@@ -2,15 +2,21 @@
 
 set -e
 
-FLYWAY_URL='https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-linux-x64.tar.gz'
+case $(uname -s) in
+Darwin)
+    FLYWAY_URL='https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-macosx-x64.tar.gz'
+
+*)
+    FLYWAY_URL='https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-linux-x64.tar.gz'
+esac
 
 if ! ( which flyway > /dev/null ); then
-    cd ${HOME}
+    pushd ${HOME}
 
-    curl -v -O ${FLYWAY_URL}
-    tar -xvzf flyway-commandline-5.0.7-linux-x64.tar.gz
+    curl -v ${FLYWAY_URL} | tar -xvz
     export PATH=${PATH}:${HOME}/flyway-5.0.7
 
+    popd
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
