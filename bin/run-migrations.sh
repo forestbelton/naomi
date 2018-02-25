702bfa2012068b668564fd4cb77cd723de50e37d
Forest Belton <forest@homolo.gy>
2018-02-24 21:58:36 -0800

    Add missing breaks to case

--- bin/run-migrations.sh
+++ bin/run-migrations.sh
@@ -5,9 +5,10 @@ set -e
 case $(uname -s) in
 Darwin)
     FLYWAY_URL='https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-macosx-x64.tar.gz'
-
+    ;;
 *)
     FLYWAY_URL='https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-linux-x64.tar.gz'
+    ;;
 esac
 
 if ! ( which flyway > /dev/null ); then
