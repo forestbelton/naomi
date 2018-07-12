b75dc92a2a698b10be8d4b3eabcb788112e969fe
Forest Belton <forest@homolo.gy>
2018-07-12 00:20:44 -0700

    Log messages on ingress

--- src/main/kotlin/com/github/forestbelton/naomi/Naomi.kt
+++ src/main/kotlin/com/github/forestbelton/naomi/Naomi.kt
@@ -2,6 +2,9 @@ package com.github.forestbelton.naomi
 
 import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import net.dv8tion.jda.core.hooks.ListenerAdapter
+import org.apache.logging.log4j.LogManager
 
 fun main(args: Array<String>) {
     val token = System.getenv("APP_TOKEN")
@@ -10,7 +13,24 @@ fun main(args: Array<String>) {
         System.exit(-1)
     }
 
-    JDABuilder(AccountType.BOT)
+    val jda = JDABuilder(AccountType.BOT)
         .setToken(token)
         .buildBlocking()
+
+    jda.addEventListener(Naomi())
+}
+
+class Naomi : ListenerAdapter() {
+    companion object {
+        val logger = LogManager.getLogger(Naomi::class.java)
+    }
+
+    override fun onMessageReceived(event: MessageReceivedEvent?) {
+        if (event == null || event.author.isBot) {
+            return
+        }
+
+        logger.info("[{}#{}] {} ({}) - {}", event.guild.name, event.channel.name, event.author.name,
+            event.author.id, event.message.contentRaw)
+    }
 }
