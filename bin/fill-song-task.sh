7ad4cf0a1fc07f88ecb51b8090b162879ce0c82c
Forest Belton <forest@homolo.gy>
2018-07-11 23:05:30 -0700

    Remove JavaScript

--- bin/fill-song-task.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-FILE_PATH=$1
-FILE_URL=$2
-
-youtube-dl --output ${FILE_PATH} --audio-format opus --audio-quality 9 -x \
-    ${FILE_URL}
--- sql/V012__remove_old_tables.sql
+++
@@ -1,5 +0,0 @@
-UPDATE users
-    SET balance = COALESCE((SELECT balance FROM user_casebucks WHERE user = users.discord_name), 0);
-
-DROP TABLE user_casebucks;
-DROP TABLE admins;
