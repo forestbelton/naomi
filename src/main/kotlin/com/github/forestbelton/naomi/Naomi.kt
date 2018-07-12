76d2bf91b2065a02ffea13f83badbfaa1cfb535f
Forest Belton <forest@homolo.gy>
2018-07-11 23:47:54 -0700

    Add main bot routine

--- src/main/kotlin/com/github/forestbelton/naomi/Naomi.kt
+++ src/main/kotlin/com/github/forestbelton/naomi/Naomi.kt
@@ -1,4 +1,16 @@
 package com.github.forestbelton.naomi
 
+import net.dv8tion.jda.core.AccountType
+import net.dv8tion.jda.core.JDABuilder
+
 fun main(args: Array<String>) {
+    val token = System.getenv("APP_TOKEN")
+    if (token == null) {
+        System.err.println("No app token available. Please set the APP_TOKEN environment variable.")
+        System.exit(-1)
+    }
+
+    JDABuilder(AccountType.BOT)
+            .setToken(token)
+            .buildBlocking()
 }
