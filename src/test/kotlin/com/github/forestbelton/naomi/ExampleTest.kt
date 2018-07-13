7b91e30b6fda0863d291faa24c1483718b99fbd9
Raas Ahsan <agro@jantox.com>
2018-07-13 01:45:22 -0500

    Add testing utilities

---
+++ src/test/kotlin/com/github/forestbelton/naomi/ExampleTest.kt
@@ -0,0 +1,13 @@
+package com.github.forestbelton.naomi
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun assertAddition() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
