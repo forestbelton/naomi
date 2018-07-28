5c2c22653362beea16c78d64e22d95638daa7d2e
Forest Belton <forest@homolo.gy>
2018-07-28 16:52:49 -0700

    Add 8ball command

--- src/main/kotlin/com/github/forestbelton/naomi/command/Commands.kt
+++ src/main/kotlin/com/github/forestbelton/naomi/command/Commands.kt
@@ -1,7 +1,9 @@
 package com.github.forestbelton.naomi.command
 
 import com.github.forestbelton.naomi.command.coinflip.CoinFlipCommand
+import com.github.forestbelton.naomi.command.eightball.EightBallCommand
 
 val allCommands = arrayOf(
-    CoinFlipCommand()
+    CoinFlipCommand(),
+    EightBallCommand()
 )
---
+++ src/main/kotlin/com/github/forestbelton/naomi/command/eightball/EightBallCommand.kt
@@ -0,0 +1,41 @@
+package com.github.forestbelton.naomi.command.eightball
+
+import com.github.forestbelton.naomi.command.Command
+import com.github.forestbelton.naomi.command.matcher.commandMatcher
+import com.github.forestbelton.naomi.message.Message
+
+import java.util.Random
+
+class EightBallCommand : Command {
+    companion object {
+        val eightBallResponses = arrayOf(
+            "it is certain.",
+            "it is decidedly so.",
+            "without a doubt.",
+            "yes, definitely.",
+            "you may rely on it.",
+            "as I see it, yes.",
+            "most likely.",
+            "outlook good.",
+            "yes.",
+            "signs point to yes.",
+            "reply hazy, try again.",
+            "ask again later.",
+            "better not tell you now.",
+            "cannot predict now.",
+            "concentrate and ask again.",
+            "don\"t count on it.",
+            "my reply is no.",
+            "my sources say no.",
+            "outlook not so good.",
+            "very doubtful."
+        )
+    }
+
+    override fun matcher(): (Message) -> Boolean = commandMatcher("8ball")
+
+    override fun execute(message: Message) {
+        val responseIndex = Random().nextInt(eightBallResponses.size - 1)
+        message.reply(eightBallResponses[responseIndex])
+    }
+}
